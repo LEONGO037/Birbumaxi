770d2192951cacba678a811b5532b34b9e0b5665
marwin <ramirezmarvinlarry@gmail.com>
2024-06-17 22:52:09 -0400

    mas avance

--- Birbumaxi/src/Controlador/menuPrincipal.java
+++ Birbumaxi/src/Controlador/menuPrincipal.java
@@ -3,15 +3,15 @@ package Controlador;
 
 import Vista.MenuGerente;
 
-//import Vista.login;
+import Vista.login;
 
 public class menuPrincipal {
 
 	public static void main(String[] args) {
-		/*login ventana1 = new login();
-		ventana1.setVisible(true);*/
-		MenuGerente menug = new MenuGerente();
-		menug.setVisible(true);
+		login ventana1 = new login();
+		ventana1.setVisible(true);
+		//MenuGerente menug = new MenuGerente();
+		//menug.setVisible(true);
 	}
 
 }

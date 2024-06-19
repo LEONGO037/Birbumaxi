ba4cf576019ed852b765d099d56829cf7d085bce
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2024-06-18 20:42:27 -0400

    Medio cliente

--- Birbumaxi/src/Modelo/ClienteFactura.java
+++ Birbumaxi/src/Modelo/ClienteFactura.java
@@ -1,11 +1,133 @@
 package Modelo;
 
-public class ClienteFactura {
-	public void ingresarCliente () {
-		
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import conexionBase.conexionBD;
+
+public class ClienteFactura extends persona{
+	
+	public int nit;
+	
+	@Override
+	public void setCorreo(String correo) {
+		// TODO Auto-generated method stub
+		super.setCorreo(correo);
+	}
+	
+	@Override
+	public void setNombre(String nombre) {
+		// TODO Auto-generated method stub
+		super.setNombre(nombre);
+	}
+	
+	public void setNit(int nit) {
+		this.nit = nit;
+	}
+	
+	@Override
+	public String getCorreo() {
+		// TODO Auto-generated method stub
+		return super.getCorreo();
+	}
+	
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return super.getNombre();
+	}
+	
+	public int getNit() {
+		return nit;
+	}
+	
+	public ClienteFactura(String nombre, String correo, int nit) {
+		super(nombre, correo);
+		this.nit = nit;
+	}
+	
+	public void agregarFactura (int idFac, int metPago) {
+		String consulta= "update factura set persona_id_persona = " + nit + ", metodo_pago = " + metPago + " where id_factura = " + idFac + ";";
+		conexionBD conec= new conexionBD();
+		Connection conn= conec.conexion();
+		PreparedStatement ps= null;
+		ResultSet rs= null;
+		try {
+			ps=conn.prepareStatement(consulta);
+			int actualizado = ps.executeUpdate();
+			if(actualizado > 0) {
+				System.out.println("Actualizado con exito");
+			} else {
+				System.out.println("No se pudo actualizar");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void ingresarClienteNuevo () {
+		String consulta= super.ingresar();
+		consulta += "persona (nombre, NIT, correo_electronico) values ('" + nombre + "', " + nit + ", '" + correo + "';";
+		conexionBD conec= new conexionBD();
+		Connection conn= conec.conexion();
+		PreparedStatement ps= null;
+		ResultSet rs= null;
+		try {
+			ps=conn.prepareStatement(consulta);
+			int aniadido = ps.executeUpdate();
+			if(aniadido > 0) {
+				System.out.println("Agregado con exito");
+			} else {
+				System.out.println("no se pudo agregar");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean buscarCliente () {
-		return true;
+		ArrayList<Integer> nits = new ArrayList<>();
+		String consulta= "SELECT nit from persona;";
+		conexionBD conec= new conexionBD();
+		Connection conn= conec.conexion();
+		PreparedStatement ps= null;
+		ResultSet rs= null;
+		try {
+			ps=conn.prepareStatement(consulta);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				int num = rs.getInt("nit");
+				nits.add(num);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		if(nits.contains(nit)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void datosEncontrados (){
+		String consulta= "SELECT nombre, correo_electronico from persona where nit = " + nit + ";";
+		conexionBD conec= new conexionBD();
+		Connection conn= conec.conexion();
+		PreparedStatement ps= null;
+		ResultSet rs= null;
+		try {
+			ps=conn.prepareStatement(consulta);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				String nombreI = rs.getString("nombre");
+				String correoI = rs.getString("correo_electronico");
+				setNombre(nombreI);
+				setCorreo(correoI);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
